8e2eb86e58ecf94610bad9ce2c8c23aacb95fadc
Adnan Mahmud <70011012+Adnan1729@users.noreply.github.com>
2023-04-17 11:58:37 +0100

    Update random_planar_graph.py
    Previous unsuccessful attempts: 
    --
    [#1]
    import networkx as nx
    import random
    import matplotlib.pyplot as plt
    
    def is_planar(G):
        try:
            nx.check_planarity(G)
            return True
        except nx.NetworkXException:
            return False
    
    def random_planar_graph(min_nodes, max_nodes):
        n = random.randint(min_nodes, max_nodes)
        G = nx.Graph()
    
        G.add_nodes_from(range(n))
    
        while True:
            a = random.choice(list(G.nodes))
            b = random.choice(list(G.nodes))
            if a != b and not G.has_edge(a, b):
                G.add_edge(a, b)
                if not is_planar(G):
                    G.remove_edge(a, b)
    
            if len(G.edges) == n * (n - 1) // 2:
                break
    
        return G
    
    if __name__ == "__main__":
        G = random_planar_graph(5, 15)
        nx.draw(G, with_labels=True)
        plt.show()
    --

--- random_planar_graph.py
+++ random_planar_graph.py
@@ -1,34 +1,25 @@
+import numpy as np
 import networkx as nx
 import random
+from scipy.spatial import Delaunay
 import matplotlib.pyplot as plt
 
-def is_planar(G):
-    try:
-        nx.check_planarity(G)
-        return True
-    except nx.NetworkXException:
-        return False
-
-def random_planar_graph(min_nodes, max_nodes):
+def random_points(min_nodes, max_nodes):
     n = random.randint(min_nodes, max_nodes)
+    points = np.random.rand(n, 2)
+    return points
+
+def delaunay_triangulation(points):
+    tri = Delaunay(points)
     G = nx.Graph()
-
-    G.add_nodes_from(range(n))
-
-    while True:
-        a = random.choice(list(G.nodes))
-        b = random.choice(list(G.nodes))
-        if a != b and not G.has_edge(a, b):
-            G.add_edge(a, b)
-            if not is_planar(G):
-                G.remove_edge(a, b)
-
-        if len(G.edges) == n * (n - 1) // 2:
-            break
-
-    return G
+    
+    for simplex in tri.simplices:
+        G.add_edges_from([(simplex[0], simplex[1]), (simplex[1], simplex[2]), (simplex[2], simplex[0])])
+    
+    return G, points
 
 if __name__ == "__main__":
-    G = random_planar_graph(5, 15)
-    nx.draw(G, with_labels=True)
+    points = random_points(100, 100)
+    G, pos = delaunay_triangulation(points)
+    nx.draw(G, pos, with_labels=True)
     plt.show()
